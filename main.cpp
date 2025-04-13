4bf1dede360f27caa1062ef91491ff5d0268afb2
Luigi31 <luigi31.yan@gmail.com>
2025-04-13 17:56:22 +0200

    Take main file in input, not only code section

--- main.cpp
+++ main.cpp
@@ -48,18 +48,24 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    std::streamsize file_size = encrypted_file.tellg();
-    encrypted_file.seekg(0, std::ios::beg);
-    uint num_uints = static_cast<uint>(file_size / 4);
-    std::vector<uint> input(num_uints);
-    std::vector<uint> output(num_uints);
+    // Start address of the code section
+    uint32_t code_offset = 0x240;
 
-    if (!encrypted_file.read(reinterpret_cast<char*>(input.data()), file_size)) {
-        std::cerr << "Could not read the encrypted file" << std::endl;
+    // Get size of the code section
+    uint32_t code_size;
+    encrypted_file.seekg(0x5C, std::ios::beg);
+    encrypted_file.read(reinterpret_cast<char*>(&code_size), sizeof(code_size));
+
+    encrypted_file.seekg(code_offset, std::ios::beg);
+    std::vector<uint> input(code_size / 4);
+    std::vector<uint> output(code_size / 4);
+
+    if (!encrypted_file.read(reinterpret_cast<char*>(input.data()), code_size)) {
+        std::cerr << "Could not read the code section" << std::endl;
         return 1;
     }
 
-    decrypt(output.data(), input.data(), static_cast<uint>(file_size));
+    decrypt(output.data(), input.data(), static_cast<uint>(code_size));
 
     std::ofstream decrypted_file(argv[2], std::ios::binary);
     if (!decrypted_file) {
@@ -67,7 +73,7 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    if (!decrypted_file.write(reinterpret_cast<char*>(output.data()), file_size)) {
+    if (!decrypted_file.write(reinterpret_cast<char*>(output.data()), code_size)) {
         std::cerr << "Failed to write the decrypted data." << std::endl;
         return 1;
     }
